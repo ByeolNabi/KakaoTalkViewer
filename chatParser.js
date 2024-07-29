b845eae850d4c9d3c22218e02bc15643801c42a9
ByeolNabi <20183338@office.deu.ac.kr>
2024-07-29 17:16:35 +0900

    Implememt chatDataMerger func

--- chatParser.js
+++ chatParser.js
@@ -1,7 +1,8 @@
 class chatDataStructor {
   constructor() {
     this.chatTXT;
-    this.imagePathList;
+    this.imageNameList;
+    this.dataAbspath;
   }
 
   /**
@@ -11,12 +12,13 @@ class chatDataStructor {
    * @returns {object[]} 사용가능한 데이터구조로 변형한 채팅 데이터를 리턴합니다.
    */
   start(folderPath) {
-    let { chatFilePath, imagePathList } = dictionaryFileLister(folderPath);
+    this.dataAbspath = folderPath;
+    let { chatFilePath, imageNameList } = dictionaryFileLister(folderPath);
 
     let parsedChatData = chatParser(chatFilePath); // chat Info 저장하기
-    let parsedImageData = imagePathParser(imagePathList); // image Info 저장하기
+    let parsedImageData = imagePathParser(imageNameList); // image Info 저장하기
 
-    let resultData = chatDataMerger(parsedImageData, parsedChatData); // image와 chat 데이터 연동하기
+    let resultData = chatDataMerger(parsedChatData, parsedImageData); // image와 chat 데이터 연동하기
     return resultData;
   }
 
@@ -24,18 +26,17 @@ class chatDataStructor {
    * 폴더에 있는 파일이름들을 분석해줍니다.
    *
    * @param {string} folderPath - 카카오톡 채팅방 zip 압축해제 폴더의 경로
-   * @returns {{ chatFilePath: string , imagePathList: string[] }} 파일 이름들 분류 데이터
+   * @returns {{ chatFilePath: string , imageNameList: string[] }} 파일 이름들 분류 데이터
    */
   dictionaryFileLister(folderPath) {
     // 폴더 리스트 쭈루룩 뽑아와서 image들 append하고 날짜 데이터 따로 저장하고 txt파일 찾아서 경로 저장하기
-    return { chatFilePath: "", imagePathList: "" };
+    return { chatFilePath: "", imageNameList: "" };
   }
 
   /**
-   * 구조화된 chatData를 드디어 리턴해줍니다.
+   * chat txt를 구조화된 chat data로 변경해줍니다. (이미지 경로 포함x)
    *
    * @param {string} chatFilePath - chat backup txt file path
-   * @param {string[]} imagePathList - image path 모음
    * @returns {Array  <{
    *   timestamp: "2024-07-22T14:36:00.000Z",
    *   sender: string,
@@ -43,12 +44,78 @@ class chatDataStructor {
    *   imageInfo: {
    *     imageTF: boolean,
    *     imagePath: string[],
-   *     imageCnt: number
+   *     imageQuantity: number
    *   }
    * }>} 구조화된 chat 데이터
    */
+  chatParser(chatFilePath) {
+    // 영차영차 파싱하자
 
-  chatParser(chatFilePath) {}
-  imagePathParser(imagePathList) {}
-  chatDataMerger(parsedImageDaarsedChatData) {}
+    return parsedChatData;
+  }
+
+  /**
+   * image파일들의 이름을 분석해서 날짜 데이터를 추가로 저장합니다.
+   *
+   * @param {Array<string>} imageNameList - path list
+   * @return {
+   *
+   * }
+   */
+  imagePathParser(imageNameList) {
+    // 파싱을 편리를 위해서 image fileName만을 따로 받았음
+    parsedImageData = []
+    for(let img_idx = 0; img_idx < imageNameList.length; ++img_idx){
+      
+      imageMETA = {path : this.dataAbspath+"", timestamp: imgDate.toISOString(), image_idx: number}
+      parsedImageData.push()
+    }
+    return parsedImageData;
+  }
+
+  /**
+   * chat data에서 비워져있던 Image path를 날짜에 맞게 연결해줍니다.
+   *
+   */
+  chatDataMerger(parsedChatData, parsedImageData) {
+    // 사진이 필요한 채팅을 찾아내고 사진과 매핑해준다.
+    // 첫 사진 날짜를 기준으로 뒷 채팅 기록들을 다 쳐낸다. ⛑️
+    resultData = parsedChatData;
+    imageFirstDate = parsedImageData[0].timestamp.slice(0, 10); // string timestamp에서 minute까지만 자른다.
+    let chat_idx = 0;
+
+    // 사진과 매핑할 채팅 시작위치 찾기
+    for (; chat_idx < parsedChatData.length; ++chat_idx) {
+      let chatDate = parsedChatData[chat_idx].timestamp.slice(0, 10);
+      if (
+        parsedChatData[chat_idx].imageInfo.imageTF &&
+        imageFirstDate == chatDate
+      ) {
+        break;
+      }
+    }
+
+    // 매핑 시작
+    for (let image_idx; image_idx < parsedImageData.legnth; ++image_idx) {
+      let imageDate = parsedImageData[image_idx].timestamp.slice(0, 10);
+      for (; chat_idx < parsedChatData.length; ++chat_idx) {
+        if (!parsedChatData[chat_idx].imageInfo.imageTF) {
+          // 이미지가 없으면 다음 chat으로 넘기기
+          continue;
+        }
+        let chatDate = parsedChatData[chat_idx].timestamp.slice(0, 10);
+        if (imageDate == chatDate) {
+          let img_cnt = parsedChatData[chat_idx].imageInfo.imageQuantity;
+          while (img_cnt--) {
+            // chat의 이미지 갯수만큼 chat의 img path에 push하기
+            parsedChatData[chat_idx].imagePath.push(
+              parsedImageData[image_idx++].path
+            );
+          }
+        }
+      }
+    }
+
+    return resultData;
+  }
 }

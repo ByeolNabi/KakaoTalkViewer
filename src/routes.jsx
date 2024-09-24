23adaf34dedfc9ba0876a889ece4cc71454462c1
ByeolNabi <20183338@office.deu.ac.kr>
2024-09-24 22:01:12 +0900

    PWA를 한 것 같습니다.

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
 import ViewerPage from "./pages/ViewerPage";
